f9914390a2bb8f4eadea9c4a5ed02557db2001df
Srinivasan R <92676627+Srinivasan8888@users.noreply.github.com>
2024-06-23 21:12:38 +0530

    ok there are some issuse with integrating with plaid, dwolla is working so far

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -7,12 +7,12 @@ import {
 } from "react-plaid-link";
 import { StyledString } from "next/dist/build/swc";
 import { useRouter } from "next/navigation";
-import { createLinkToken } from "@/lib/actions/user.actions";
+import { createLinkToken, exchangePublicToken } from "@/lib/actions/user.actions";
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
   const router = useRouter();
   const onSuccess = useCallback<PlaidLinkOnSuccess>(
-    async (public_token: String) => {
+    async (public_token: string) => {
         await exchangePublicToken({ publicToken: public_token, user })
       router.push("/");
     },
