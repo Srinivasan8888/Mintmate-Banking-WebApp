1f41e9ebc79a583cab0ce99976cf0a69906cbf7d
Srinivasan R <92676627+Srinivasan8888@users.noreply.github.com>
2024-07-02 19:45:05 +0530

    can able to fetxh a data from the appwrite to the tabs in recent transactions so far

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,19 +1,42 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { BankTabItem } from "./BankTabItem";
+import BankInfo from "./BankInfo";
 
-const RecentTransactions = ({accounts, transactions =[], appwriteItemId,  page = 1} : RecentTransactionsProps) => {
+const RecentTransactions = ({
+  accounts,
+  transactions = [],
+  appwriteItemId,
+  page = 1,
+}: RecentTransactionsProps) => {
   return (
-    <section className='recent-transaction'>
-        <header className='flex item-center justify-between'>
-            <h2 className='recent-transcation-label'>
-                Recent Transaction
-            </h2>
-            <Link href={'/transaction-history/?id=${appwriteItemId}'} className='view-all-btn'>
-            View all
-            </Link>
-        </header>
-    </section>
-  )
-}
+    <section className="recent-transaction">
+      <header className="flex item-center justify-between">
+        <h2 className="recent-transcation-label">Recent Transaction</h2>
+        <Link
+          href={"/transaction-history/?id=${appwriteItemId}"}
+          className="view-all-btn"
+        >
+          View all
+        </Link>
+      </header>
 
-export default RecentTransactions
+      <Tabs defaultValue={appwriteItemId} className="w-full">
+        <TabsList className="recent-transactions-tablist">
+          {accounts.map((account: Account) => <TabsTrigger key={account.id} value={account.appwriteItemId}>
+            <BankTabItem key={account.id} account={account} appwriteItemId={appwriteItemId}/>
+          </TabsTrigger>)}
+        </TabsList>   
+
+        {accounts.map((account: Account) => (
+            <TabsContent value={account.appwriteItemId} key={account.id} className="space-y-4">
+                <BankInfo account={account} appwriteItemId={appwriteItemId} type="full"/>
+            </TabsContent>
+        ))}
+      </Tabs>
+    </section>
+  );
+};
+
+export default RecentTransactions;
